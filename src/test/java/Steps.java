beca245ded04fe836d95a0bca44034ca551f8e6a
d.avramenko <d.avramenko@prequelapp.com>
2022-06-29 11:16:41 +0700

    add new test for allure

--- src/test/java/Steps.java
+++ src/test/java/Steps.java
@@ -1,16 +1,11 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.commands.TakeScreenshot;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import java.nio.charset.StandardCharsets;
-
 import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
